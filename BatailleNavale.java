16af3a1837b8d6a7ed6dd833bb32aeab23ae9cc5
gesnouin-kevin <gesnouin.kevin@gmail.com>
2015-01-09 10:16:23 +0100

    Ajout Kevin - Abdelsalem

--- BatailleNavale.java
+++ BatailleNavale.java
@@ -1,7 +1,24 @@
 
 public class BatailleNavale {
 	
-
+	public static void main(String [ ] args)
+	{
+		
+		Partie partie;
+		partie = new Partie();
+		
+		partie.placerBateau(partie.getVictime());
+		
+		partie.lancerBombeS(partie.getAttaquant());
+		
+		
+		
+		
+		
+		
+	}
+	
+	
 	
 
 }
