6902f41a73d2dc9d4f4b2e441b7b083fcade4b43
BillalH <billalhatoum@gmail.com>
2015-01-09 10:13:09 +0100

    Revert "Ajout Kevin - Abdelsalem"
    This reverts commit 8c680352b6622fd6523b1479f02fcbc0451c8df1.

--- BatailleNavale.java
+++ BatailleNavale.java
@@ -1,24 +1,7 @@
 
 public class BatailleNavale {
 	
-	public static void main(String [ ] args)
-	{
-		
-		Partie partie;
-		partie = new Partie();
-		
-		partie.placerBateau(partie.getVictime());
-		
-		partie.lancerBombeS(partie.getAttaquant());
-		
-		
-		
-		
-		
-		
-	}
-	
-	
+
 	
 
 }
