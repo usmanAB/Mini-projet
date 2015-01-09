8c680352b6622fd6523b1479f02fcbc0451c8df1
BillalH <billalhatoum@gmail.com>
2015-01-09 10:07:00 +0100

    Ajout Kevin - Abdelsalem

--- Bateau.java
+++ Bateau.java
@@ -1,7 +1,25 @@
 
 public class Bateau {
 	
+	private int taille;
 	
+	public Bateau()
+	{
+		this.taille = 0;
+	}
+
+	public Bateau(int taille)
+	{
+		this.taille=taille;
+	}
+
+	public int getTaille() {
+		return taille;
+	}
+
+	public void setTaille(int taille) {
+		this.taille = taille;
+	}
 
 	
 }
